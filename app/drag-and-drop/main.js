a398fdfd04f7e12408e93a5a415d1df1013e4cce
Peter Ajtai <peter.ajtai@thinksolid.com>
2014-03-05 07:55:32 -0800

    further requirejs updates for jquery ui

--- app/drag-and-drop/main.js
+++ app/drag-and-drop/main.js
@@ -2,21 +2,12 @@
     'use strict';
 
     require.config({
-        shim : {
-            jplugins : {
-                dependencies : ['jquery']
-            },
-            jmain : {
-                dependencies: ['jquery', 'jplugins']
-            }
-        },
         paths : {
             jquery : '../vendor/jquery/dist/jquery',
             rivets : '../vendor/rivets/dist/rivets',
             backbone : '../vendor/backbone-amd/backbone',
             text : '../vendor/requirejs-text/text',
-            jplugins : './js/jquery-1.10.2',
-            jmain : './js/jquery-ui-1.10.4.custom.min'
+            jqueryui : './js/jquery-ui-1.10.4.custom.min'
         },
         packages : [
             {
