49d19d5b0ea9e3353ad83d41f2041676ee9471fb
Peter Ajtai <peter.ajtai@thinksolid.com>
2014-03-05 07:09:03 -0800

    updating to use a collection

--- app/drag-and-drop/drag-and-drop-view/view.js
+++ app/drag-and-drop/drag-and-drop-view/view.js
@@ -1,34 +1,37 @@
-define(['masseuse', 'underscore', './options', 'jquery', 'jmain'], function(masseuse, _, options, $) {
+define(['backbone', 'masseuse', 'underscore', './options', 'jquery', 'jmain'],
+    function(Backbone, masseuse, _, options, $) {
     'use strict';
 
-    return masseuse.plugins.rivets.RivetsView.extend({
+    var DragDrop = masseuse.plugins.rivets.RivetsView.extend({
         defaultOptions :  options,
+        initialize : initialize,
         afterRender : afterRender
     });
 
+    function initialize() {
+        this.collection = new Backbone.Collection(
+            [{type:'apple'},{type:'banana'},{type:'kiwi'},{type:'grapefruit'},{type:'grape'}]);
+        DragDrop.__super__.initialize.apply(this, arguments);
+    }
+
     function afterRender() {
         var $sortable = this.$( '#sortable');
-
         $sortable.sortable({
             stop : _.debounce(_stop.bind(this, $sortable), 50)
         });
     }
 
+    return DragDrop;
+
     function _stop($sortable) {
         var fruits = [],
-            changed = false;
+            self = this;
 
         $sortable.find('li').each(function() {
-            var fruit = $(this).text().trim();
-            if (fruit.length) {
-                fruits.push(fruit);
-                changed = true;
-            }
+            fruits.push(self.collection.get(this.id));
         });
 
-        if (changed) {
-            this.model.set('fruits', fruits);
-            this.refresh();
-        }
+        this.collection.set(fruits);
+        this.refresh();
     }
 });
