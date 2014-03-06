f323675d20b2c2e103ec37f028cb1db30a416e90
Peter Ajtai <peter.ajtai@thinksolid.com>
2014-03-06 05:50:05 -0800

    Build: 14 Branch: master  0.2.1 SHA: 9cac437

--- drag-and-drop/main.js
+++ drag-and-drop/main.js
@@ -2,18 +2,12 @@
     'use strict';
 
     require.config({
-        shim : {
-            touchpunch : {
-                deps : ['jqueryui']
-            }
-        },
         paths : {
             jquery : '../vendor/jquery/dist/jquery',
             rivets : '../vendor/rivets/dist/rivets',
             backbone : '../vendor/backbone-amd/backbone',
             text : '../vendor/requirejs-text/text',
-            jqueryui : './js/jquery-ui-1.10.4.custom.min',
-            touchpunch : './js/touch-punch'
+            jqueryui : './js/jquery-ui-1.10.4.custom.min'
         },
         packages : [
             {
@@ -30,8 +24,7 @@
     require([
         'drag-and-drop-view/view',
         'jquery'
-    ], function (DragDrop, $) {
-        window.$ = $;
+    ], function (DragDrop) {
         new DragDrop().start();
     });
 }());
